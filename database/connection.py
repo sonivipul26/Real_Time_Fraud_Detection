9b5da53c806a0a2476499bb07f698661a5315c87
sonivipul26 <vipulktp26@gmail.com>
2026-07-14 18:56:33 +0530

    Build production FastAPI backend with PostgreSQL transaction persistence

--- database/connection.py
+++ database/connection.py
@@ -1,14 +1,33 @@
+import os
+
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # ==========================================
+# Load Environment Variables
+# ==========================================
+
+load_dotenv()
+
+# ==========================================
 # Database Configuration
 # ==========================================
 
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
 DATABASE_URL = (
-    "postgresql://postgres:vipul@localhost:5432/fraud_detection"
+    f"postgresql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
+
 
 # ==========================================
 # SQLAlchemy Engine
